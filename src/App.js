6c5fdbd6848b15d6b31627bdbc787fb55a3ea7b6
atulit gaur <atulit23@gmail.com>
2024-07-08 23:05:34 +0530

    first part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
+import Services from './components/Services';
+import Prices from './components/Prices';
+import Quotes from './components/Quotes';
+import OurStory from './components/OurStory';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
+      <Services />
+      <Prices />
+      <Quotes />
+      <OurStory />
+      <Services />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styles from '../css/main.module.css'
+import AI from '../assets/images/Group 11.png'
+
+export default function Main() {
+    return (
+        <div className={styles.main}>
+            <div className={styles.inner__component}>
+                <div className={styles.navbar}>
+                    <div className={styles.normal}>Home</div>
+                    <div className={styles.normal}>Services</div>
+                    <div className={styles.normal}>Pricing</div>
+                    <div className={styles.all__stylish}>
+                        <div className={styles.stylish}>
+                            <div className={styles.triangle_right}></div>
+                            <div className={styles.stylish_text}>Xander</div>
+                            <div className={styles.triangle_left}></div>
+                        </div>
+                    </div>
+                    <div className={styles.normal}>Quotes</div>
+                    <div className={styles.normal}>Our Story</div>
+                    <div className={styles.normal}>Signup</div>
+                </div>
+                <div className={styles.main__content}>
+                    <div className={styles.main__text}>
+                        <div className={styles.upper__text}>
+                            Focus on Goals, Not the
+                            <span className={styles.gradient}>Tech</span>
+                        </div>
+                        <div className={styles.lower__text}>
+                            Say goodbye to technical headaches – from data preprocessing to model deployment, we automate everything. No tech skills? No problem. Embrace the future of AI with simplicity and ease.
+                        </div>
+                        <div className={styles.explore__now}>
+                            Explore now
+                        </div>
+                    </div>
+                    <div className={styles.main__image}>
+                        <img src={AI} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
