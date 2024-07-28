ade547416860f73fb7fff62d18648fd40f36b689
Developing-wizard <134376039+Raman413@users.noreply.github.com>
2024-07-29 00:10:33 +0530

    008

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -90,7 +90,7 @@ export default function Main() {
                     <div className={styles.normal} onClick={() => {
                         document.getElementById("footer").scrollIntoView({ behavior: 'smooth' })
                     }}>Contact Us</div>
-                    <div className={styles.normal1} onClick={() => {
+                    <div className={styles.signup} onClick={() => {
                         setShow(!show)
                     }}>Signup</div>
                 </div>
